faf62752176d1d900548f1ecd324a8c45433987e
Miguel Ángel Riascos <73512305+dervalinor@users.noreply.github.com>
2023-03-14 16:28:10 +0000

    Display value frequency functional groups IR

--- 4_IR_spectrum/ir_spectrum_vector.cpp
+++ 4_IR_spectrum/ir_spectrum_vector.cpp
@@ -14,37 +14,37 @@ vector<string> findFunctionalGroups(vector<double> peaks){
         // Check if the peak falls within the range for Alkenes
         //concatenating the original string with the value of "peak", using the "std::to_string" function.
         if (peak >= 2850 && peak <= 2970) {
-            functionalGroups.push_back("Alkenes -- "  + std::to_string(peak)); 
+            functionalGroups.push_back("Alkenes "  + std::to_string(peak)); 
         }
         
         // Check if the peak falls within any of the ranges for Alkenes (-C=C - H)
         if ((peak >= 1340 && peak <= 1470) || (peak >= 3010 && peak <= 3095) || (peak >= 675 && peak <= 995)) {
-            functionalGroups.push_back("Alkenes (-C=C - H) -- " + std::to_string(peak));
+            functionalGroups.push_back("Alkenes (-C=C - H) " + std::to_string(peak));
         }
         
         // Check if the peak is equal to the value for Alkynes (-C≡C- H)
         if (peak == 3300) {
-            functionalGroups.push_back("Alkynes (-C≡C- H) -- " + std::to_string(peak));
+            functionalGroups.push_back("Alkynes (-C≡C- H) "+ std::to_string(peak));
         }
         
         // Check if the peak falls within any of the ranges for Aromatic rings
         if ((peak >= 3010 && peak <= 3100) || (peak >= 690 && peak <=900)) {
-            functionalGroups.push_back("Aromatic rings -- " + std::to_string(peak));
+            functionalGroups.push_back("Aromatic rings " + std::to_string(peak));
         }
         
         // Check if the peak falls within any of the ranges for Monomeric alcohols, phenols or Hydrogen-bonded alcohols, phenols
         if ((peak >=3590 && peak <=3650) || (peak >=3200 && peak <=3600)) {
-            functionalGroups.push_back("Monomeric alcohols, phenols or Hydrogen-bonded alcohols, phenols -- " + std::to_string(peak));
+            functionalGroups.push_back("Monomeric alcohols, phenols or Hydrogen-bonded alcohols, phenols " + std::to_string(peak));
         }
         
          // Check if the peak falls within any of the ranges for Monomeric carboxylic acids or Hydrogen-bonded carboxylic acids
          if ((peak >=3500 && peak <=3650) || (peak >=2500 && peak<=2700)) {
-             functionalGroups.push_back("Monomeric carboxylic acids or Hydrogen-bonded carboxylic acids -- " + std::to_string(peak));
+             functionalGroups.push_back("Monomeric carboxylic acids or Hydrogen-bonded carboxylic acids " + std::to_string(peak));
          }
 
          // Check if the value falls within range for Amines or amides 
          if(peak>=3300&&peak<=3500){
-             functionalGroups.push_back("Amines or amides -- " + std::to_string(peak));
+             functionalGroups.push_back("Amines or amides " + std::to_string(peak));
          } 
     }  
 
@@ -53,20 +53,10 @@ vector<string> findFunctionalGroups(vector<double> peaks){
 }
 
 int main() {
-    
-    int n;
-    // Define a vector of absorption peaks
-    // Declare a variable to store each user input 
+
     double userInput;
     vector<string> testFunctionalGroups;
-    vector<double> peaks; //no use peaks(n), create problem in memory
-
-    cout << "How many peaks do you want to enter?: "; cin >> n;
-
-    for(int i = 0; i < n; i++){
-        cout << "Enter value #" << i << ":"; cin >> userInput; 
-        peaks.push_back(userInput);//It makes the vector grow dynamically
-    }
+    vector<double> peaks = {468.70, 524.64, 603.72, 682.80, 700.16, 746.45, 779.24, 848.68, 914.26, 948.98, 1024.20, 1074.35, 1161.15, 1226.73, 1305.81, 1409.96, 1508.33, 1560.41, 1625.99, 2117.84, 2360.87, 2964.59, 3037.89};
 
     testFunctionalGroups = findFunctionalGroups(peaks);
     
