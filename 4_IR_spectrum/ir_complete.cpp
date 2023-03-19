819eb0edde5bb86003bcff260ab4d2e37a1e447e
dervalinor <dervalinor@protonmail.com>
2023-03-18 22:18:05 -0500

    Comment code about git update

--- 4_IR_spectrum/ir_complete.cpp
+++ 4_IR_spectrum/ir_complete.cpp
@@ -34,7 +34,18 @@ In the main function, the code defines a vector of peaks and a vector of functio
 their corresponding frequency ranges. It then loops through the peaks vector, calling the identifyGroup function for each peak and printing the result. If the peak matches a functional group, the code also prints the intensity of the peak for that group. If the peak does not match any group, 
 the code prints a message indicating that it does not match any functional group in the table.
 
-*/
+Note:Git version 2.23 introduced two new commands: `git switch` and `git restore`. These commands were introduced to split the functionality of the `git checkout` command into two separate commands⁴.
+
+- `git switch`: This command is used to switch between branches or restore working tree files⁴.
+
+- `git restore`: This command is used to restore working tree files or unstage changes⁴.
+
+These are relatively new commands that were introduced in August 2019. Is there anything else you would like me to explain?
+
+ソース: Bing との会話 3/18/2023(1) Git 2.23 released with two new commands ‘git switch’ and ‘git restore .... https://hub.packtpub.com/git-2-23-released-with-two-new-commands-git-switch-and-git-restore-a-new-tutorial-and-much-more/ アクセス日時 3/18/2023.
+(2) Git 2.23 released with two new commands ‘git switch’ and ‘git restore .... https://bing.com/search?q=new+git+commands アクセス日時 3/18/2023.
+(3) Basic GIT Commands: A Complete Cheat Sheet for Beginners. https://www.hostinger.com/tutorials/basic-git-commands アクセス日時 3/18/2023.
+(4) Git cheatsheet - GitHub Docs. https://docs.github.com/en/get-started/quickstart/git-cheatsheet アクセス日時 3/18/2023. */
 
 int main() {
 
